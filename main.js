aa39c0312b13c263652e0a23b38222483273ae45
Vinicius Barbosa <yuribarbosa48@hotmail.com>
2023-02-01 21:12:36 -0300

    adicionando animacao no scroll

--- main.js
+++ main.js
@@ -67,6 +67,28 @@ function initScrollSuave(){
 }
 initScrollSuave();
 
+const sections = document.querySelectorAll('.js-scroll')
+
+function initAnimacaoScroll () {
+    if(sections.length){
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll () {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0
+                if(isSectionVisible) {
+                    section.classList.add('ativo')
+                    
+                }
+            })
+        }
+        animaScroll();
+        window.addEventListener('scroll', animaScroll)
+    }
+}
+initAnimacaoScroll();
+
 
 
 
